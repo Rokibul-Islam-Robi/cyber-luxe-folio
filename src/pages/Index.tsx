170be1ff030bb83308cef566bc02a2200a011e9e
Rokibul Islam Robi <149813484+Rokibul-Islam-Robi@users.noreply.github.com>
2025-07-31 23:42:51 +0600

    Remove loading screen and optimize Vercel deployment

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,20 +1,7 @@
-import { useState, useEffect } from 'react';
-import Preloader from '../components/Preloader';
 import Portfolio from './Portfolio';
 
 const Index = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handlePreloaderComplete = () => {
-    setIsLoading(false);
-  };
-
-  return (
-    <>
-      {isLoading && <Preloader onComplete={handlePreloaderComplete} />}
-      <Portfolio />
-    </>
-  );
+  return <Portfolio />;
 };
 
 export default Index;
